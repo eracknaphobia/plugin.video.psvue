09595ed946b9929f12f5b12a69d9dfe0281fd729
snoopyjoe <tgarner14@gmail.com>
2018-02-07 04:27:22 -0800

    Update Inputstream adaptive condition
    Update condition check for when inputstream adaptive is enabled.

--- resources/lib/ps_vue.py
+++ resources/lib/ps_vue.py
@@ -431,7 +431,7 @@ def get_stream(url, airing_id, channel_id, program_id, series_id, tms_id, title,
 
     inputstreamCOND = str(json_source['body']['dai_method']) # Checks whether stream method is "mlbam" or "freewheel"
 
-    if  inputstreamCOND == 'mlbam' and xbmc.getCondVisibility('System.HasAddon(inputstream.adaptive)'):
+    if  inputstreamCOND != 'freewheel' and xbmc.getCondVisibility('System.HasAddon(inputstream.adaptive)'):
         stream_url = json_source['body']['video_alt'] # Uses alternate Sony stream to prevent Inputstream adaptive from crashing
         listitem.setProperty('inputstreamaddon', 'inputstream.adaptive')
         listitem.setProperty('inputstream.adaptive.manifest_type', 'hls')
