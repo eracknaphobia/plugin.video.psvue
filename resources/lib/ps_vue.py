e08e743924bc31be9c4ea9059aa014f679c8f56e
snoopyjoe <tgarner14@gmail.com>
2018-03-12 01:49:34 -0700

    Test UTC time
    UTC time is off by 7hrs on some builds.

--- resources/lib/ps_vue.py
+++ resources/lib/ps_vue.py
@@ -549,8 +549,7 @@ def create_device_id():
 def utc_to_local(utc_dt):
     # get integer timestamp to avoid precision lost
     timestamp = calendar.timegm(utc_dt.timetuple())
-    #local_dt = datetime(1970, 1, 1) + timedelta(seconds=timestamp)
-    local_dt = datetime.fromtimestamp(timestamp)
+    local_dt = datetime(1970, 1, 1) + timedelta(seconds=timestamp-25200)
     assert utc_dt.resolution >= timedelta(microseconds=1)
     return local_dt.replace(microsecond=utc_dt.microsecond)
 
