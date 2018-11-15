28e7ab8f81d439bdf9b5dd2b5e1add288e58aa0e
snoopyjoe <tgarner14@gmail.com>
2018-11-15 02:24:14 -0800

    Testing--->Master 4 (#6)
    * All Channels and Live TV would ask for DVR or VOD selection when opened.
    
    * Syntax errors

--- main.py
+++ main.py
@@ -127,6 +127,17 @@ elif mode == 900:
 
     get_stream(url, airing_id, channel_id, program_id, series_id, tms_id, title, plot, icon)
 
+elif mode == 950:
+    if vod != airing_id:
+        choice = xbmcgui.Dialog().yesno("Where would you like to watch this episode?","Click an item below to choose your preference", nolabel='ON DEMAND', yeslabel='DVR')
+    	if choice:
+            get_stream(url, airing_id, channel_id, program_id, series_id, tms_id, title, plot, icon)
+    	else:
+            airing_id = vod
+            url = 'https://media-framework.totsuko.tv/media-framework/media/v2.1/stream/airing/' + vod
+            get_stream(url, airing_id, channel_id, program_id, series_id, tms_id, title, plot, icon)
+    get_stream(url, airing_id, channel_id, program_id, series_id, tms_id, title, plot, icon)
+
 elif mode == 997:
     epg_service = xbmcaddon.Addon('service.psvue.epg')
     epg_file_path = xbmc.translatePath(epg_service.getSetting(id='location'))
